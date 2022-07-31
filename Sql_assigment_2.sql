61429ea98c20c8731a49c822c99a3ac29406c3a3
satwikbannikal <106804919+satwikbannikal@users.noreply.github.com>
2022-07-30 20:12:43 -0700

    Add files via upload

---
+++ Sql_assigment_2.sql
@@ -0,0 +1,32 @@
+SHOW DATABASES;
+
+CREATE DATABASE ORG;
+SHOW DATABASES;
+USE ORG;
+
+Create table Worker (
+	WORKER_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY INT(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25)
+);
+
+INSERT INTO Worker 
+	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
+		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
+		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
+		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
+		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
+		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
+		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
+		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
+		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+select * from worker
+
+ Select substring(FIRST_NAME,1,3) from Worker;
+ Select INSTR(FIRST_NAME, BINARY'a') from Worker where FIRST_NAME = 'Amitabh';
+ SELECT t.DEPARTMENT,t.FIRST_NAME,t.Salary from(SELECT max(Salary) as TotalSalary,DEPARTMENT from Worker group by DEPARTMENT) as TempNew 
+Inner Join Worker t on TempNew.DEPARTMENT=t.DEPARTMENT 
+ and TempNew.TotalSalary=t.Salary;
---
+++ Sql_assigment_6.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE ORG;
+SHOW DATABASES;
+USE ORG;
+
+CREATE TABLE Worker (
+	WORKER_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY INT(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25),
+    EMAIL VARCHAR(100)
+);
+
+INSERT INTO Worker 
+	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT,EMAIL) VALUES
+		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR','monikaarora1@gmail.com'),
+		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin','niharikaarora1@gmail.com'),
+		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR','vishalsinghal1@gmail.com'),
+		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin','amitabhsingh1@gmail.com'),
+		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin','vivekbhati1@gmail.com'),
+		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account','vipuldiwan1@gmail.com'),
+		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account','satishkumar1@gmail.com'),
+		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin','geetikachauhan1@gmail.com');
+
+Select * from Worker where year(JOINING_DATE) = 2014 and month(JOINING_DATE) = 2;
+SELECT WORKER_TITLE, AFFECTED_FROM, COUNT(*)
+FROM Title
+GROUP BY WORKER_TITLE, AFFECTED_FROM
+HAVING COUNT(*) > 1;
+
